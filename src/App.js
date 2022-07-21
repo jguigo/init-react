c4aa2f82a6955dd6fb1e4727d35011d5fb9033f2
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-20 21:26:08 -0300

    Intro of react without JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+
+const titulo = <h1>Esse é um titulo</h1>;
 
 const App = () => {
-   return <div>Meu app react</div>;
-}
+
+   const ativo = true;
+   const random = Math.random();
+
+   function mostrarNome(sobrenome) {
+      return "Guilherme " + sobrenome;
+   }
+
+   const carro = {
+      marca: "Ford",
+      rodas: "4",
+   };
+
+   const estiloP = {
+      color: 'blue',
+      fontSize: "2.5rem"
+
+   }
+
+   return (
+      <>
+         {titulo}
+         <p>
+            {true ? "teste" : "testen"} - {10}
+            {mostrarNome("Melo")}
+         </p>
+         <p style={estiloP}>{new Date().getFullYear()}</p>
+         <p>{carro.marca}</p>
+         <p>{carro.rodas}</p>
+         <p className={ativo ? "ativo" : "inativo"}>{random * 1000}</p>
+      </>
+   );
+};
 
 export default App;
