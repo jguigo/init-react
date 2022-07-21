54e7feb8b99658a8281ac7d91c3a20c084fb1361
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-21 20:22:54 -0300

    Events

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import React from "react";
 
-const produtos = [
-   {
-      id: 1,
-      nome: "Smartphone",
-      preco: "R$ 2000",
-      cores: ["#29d8d5", "#252a34", "#fc3766"],
-   },
-   {
-      id: 3,
-      nome: "Notebook",
-      preco: "R$ 3000",
-      cores: ["#ffd045", "#d4394b", "#f37c59"],
-   },
-   {
-      id: 3,
-      nome: "Tablet",
-      preco: "R$ 1500",
-      cores: ["#365069", "#47c1c8", "#f95786"],
-   },
-];
-
 const App = () => {
-   const dados = produtos.filter(({ preco }) =>
-      Number(preco.replace("R$ ", "") > 1500)
-   );
+   function handleClick(event) {
+      console.log(event);
+   }
+
+   function handleScroll(event) {
+      console.log(event);
+   }
+
+   window.addEventListener("scroll", handleScroll);
 
    return (
-      <section>
-         {dados.map(({ id, nome, preco, cores }) => (
-            <div key={id}>
-               <h1>{nome}</h1>
-               <p>Preço: {preco}</p>
-               <ul>
-                  {cores.map((cor) => (
-                     <li
-                        key={cor}
-                        style={{ backgroundColor: cor, color: "#fff" }}>
-                        {cor}
-                     </li>
-                  ))}
-               </ul>
-            </div>
-         ))}
-      </section>
+      <div style={{height: "2000px"}}>
+         <button onClick={(event) => alert(event.target.innerText)}>
+            Click
+         </button>
+      </div>
    );
 };
 export default App;
