ba4657d8d17c5f7c14066e5821d0197d0296826d
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-21 23:21:19 -0300

    Exerc2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
-import Form from "./Form/Form";
-
-const Titulo = (props) => {
-   return <h1 style={{ color: props.cor }}>{props.texto}</h1>;
-};
-
-const Titulo2 = (props) => {
-   return <h1>{props.children}</h1>;
-};
-
-const Titulo3 = (props) => {
-   return (
-      <h1 style={{ color: props.cor }}>
-         {props.texto}, {props.children}
-      </h1>
-   );
-};
+import Header from "./pages/Header";
+import Home from "./pages/Home";
+import Produtos from "./pages/Produtos";
 
 const App = () => {
+   const { pathname } = window.location;
+   console.log(pathname);
    return (
-      <div>
-         <Titulo cor="blue" texto="Testando1" />
-         <Titulo2>Testando children</Titulo2>
-         <Titulo3 cor="green" texto="Misturando o props">com o children</Titulo3>
-         <Form />
-      </div>
+      <>
+         <Header />
+         {pathname === "/" ? <Home /> : <Produtos />}
+      </>
    );
 };
 export default App;
