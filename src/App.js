8fc93bceea0569d50e05b560e59184ef6a630a3c
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-22 15:29:17 -0300

    only modal

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import ButtonModal from "./ButtonModal";
 import { Modal } from "./Modal";
 
 const App = () => {
-   let [ativo, setAtivo] = React.useState(false);
-   const [dados, setDados] = React.useState({ nome: "Guilherme", idade: 27 });
-   const [modal, setModal] = React.useState(false);
 
-   function handleClick() {
-      setAtivo(!ativo);
-      setDados({ ...dados, faculdade: "UNISSAU" });
-   }
+   const [modal, setModal] = React.useState(false);
 
    return (
       <div>
-         <div>
-            <p>{dados.nome}</p>
-            <p>{dados.idade}</p>
-            <p>{dados.faculdade}</p>
-            <button onClick={handleClick}>{ativo ? "ativo" : "inativo"}</button>
-         </div>
          <Modal modal={modal} setModal={setModal} />
          <ButtonModal setModal={setModal} />
       </div>
