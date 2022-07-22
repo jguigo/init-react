46daeff777bb048a694c86843abb0e7024c1a74f
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-21 21:36:10 -0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
-import Footer from "./Footer";
 import Form from "./Form/Form";
-import Header from "./Header";
 
-const Teste = () => {
-   const active = true;
-   if (active) {
-      return <p>Teste</p>;
-   }
-   else {
-      return null
-   }
+const Titulo = (props) => {
+   return <h1 style={{ color: props.cor }}>{props.texto}</h1>;
+};
 
+const Titulo2 = (props) => {
+   return <h1>{props.children}</h1>;
+};
+
+const Titulo3 = (props) => {
+   return (
+      <h1 style={{ color: props.cor }}>
+         {props.texto}, {props.children}
+      </h1>
+   );
 };
 
 const App = () => {
    return (
-      <>
-         <Teste />
-         <Header />
+      <div>
+         <Titulo cor="blue" texto="Testando1" />
+         <Titulo2>Testando children</Titulo2>
+         <Titulo3 cor="green" texto="Misturando o props">com o children</Titulo3>
          <Form />
-         <Footer />
-      </>
+      </div>
    );
 };
 export default App;
