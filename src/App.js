8724de31649634c2fb7c6f0a3a755d15e65bb16b
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-23 17:26:42 -0300

    identation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from "react";
 
 const App = () => {
-   const [contar, setContar] = React.useState(0);
-   const [items, setItems] = React.useState([]);
-
-   function handleClick() {
-      setContar((contar) => contar + 1);
-      setItems((items) => [...items, "Item " + (contar + 1)]);
-   }
-
-   return (
-      <div>
-         <button onClick={handleClick}>{contar}</button>
-         {items.map((item) => (
-            <li key={item}>{item}</li>
-         ))}
-      </div>
-   );
+   return <div></div>;
 };
 
 export default App;
